20d67cce9e6fcf1085fe4b5f522e4d019659c6ce
picardcharlie <picardcharlie@gmail.com>
2023-12-22 16:14:11 +0800

    added subdirectory sorting

--- photo_sort.py
+++ photo_sort.py
@@ -24,33 +24,40 @@ if not os.path.exists(cwd + "/memes"):
 else:
     print("meme folder already exists, not creating")
 
+
+meme_directory = cwd + "/memes"
+
+files_moved = 0
+
 def photo_regex(file_name):
     pattern = re.compile(r"\d\d\d\d\d\d\d\d_\d\d\d\d\d\d\.jpg", re.IGNORECASE)
     return pattern.match(file_name)
 
-<<<<<<< HEAD
-=======
-files_moved = 0
+def check_picture(file_name, current_directory):
+    # to use variable outside of function, use global at start of function
+    global files_moved
+    if bool(photo_regex(file_name)) == False:
+        if file_name == "memes" or file_name == "photo_sort.py" or os.path.isdir(file_name):
+            print(f"don't move this {file_name}")
+        else:
+            shutil.move((current_directory + "/" + file_name), (meme_directory + "/" + file_name))
+            files_moved += 1
 
->>>>>>> 5a5dab6 (added total operations done)
+
 #print(os.listdir(cwd))
 # begin looping through all files inside of cwd.
 for file in os.listdir(cwd):
 
-    # keep track of how many files are moved (future update).
+	# if a directory, go inside and send any dankness to memes.
+    if 	os.path.isdir(file):
+        temp_work_dir = cwd + "/" + file
+        for files in os.listdir(temp_work_dir):
+            check_picture(files, temp_work_dir)
+
 
     # check the name of the file against regex
     # if matches camera photo, ignore and go onto the next
     # else, move file to meme folder
-    if bool(photo_regex(file)) == False:
-        if file == "memes" or file == "photo_sort.py":
-            print(f"don't move this {file}")
-        else:
-            shutil.move((cwd + "/" + file), (cwd + "/memes/" + file))
-<<<<<<< HEAD
-=======
-            files_moved += 1
+    check_picture(file, cwd)
 
 print(f"Operation complete, {files_moved} dank memes and others sorted out.")
->>>>>>> 5a5dab6 (added total operations done)
-
